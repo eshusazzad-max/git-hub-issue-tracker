501dc86e765335484011c54106c74bdc374ea6ad
eshusazzad-max <eshusazzad@gmail.com>
2026-03-09 09:09:31 +0600

    searchissue function added in js

--- script.js
+++ script.js
@@ -9,19 +9,28 @@ const API_URL = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
 let allIssues = []
 
 async function loadIssues(){
-
 loadingSpinner.classList.remove("hidden")
-
 const res = await fetch(API_URL)
-
 const data = await res.json()
-
 allIssues = data.data
-
 renderIssues(allIssues)
-
 loadingSpinner.classList.add("hidden")
 
 }
 
 loadIssues()
+
+async function searchIssues(){
+const text = searchInput.value
+if(text === ""){
+renderIssues(allIssues)
+return
+}
+const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`)
+const data = await res.json()
+renderIssues(data.data)
+}
+searchBtn.onclick = () => {
+searchIssues()
+
+}
